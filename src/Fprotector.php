3c0e5c717c873d5d93b2d3254dc7c9562ef10b35
floor12 <floor12@floor12.net>
2020-04-16 14:06:03 +0200

    Check method now returns bool value or throws exception.

--- src/Fprotector.php
+++ src/Fprotector.php
@@ -13,7 +13,10 @@ use yii\web\BadRequestHttpException;
 
 class Fprotector
 {
-
+    /**
+     * @param $modelName
+     * @return string
+     */
     static public function checkScript($modelName)
     {
         $view = \Yii::$app->view;
@@ -22,12 +25,17 @@ class Fprotector
         return "<input type='hidden' id='{$modelName}_code' name='{$modelName}[code]' value='" . md5($_SERVER['REMOTE_ADDR']) . "'>";
     }
 
-
+    /**
+     * @param $modelName
+     * @return bool
+     * @throws BadRequestHttpException
+     */
     static public function check($modelName)
     {
         if (!YII_ENV_TEST && (!isset($_POST[$modelName]['code']) || $_POST[$modelName]['code'] != md5(md5($_SERVER['REMOTE_ADDR'])))) {
             throw new BadRequestHttpException('Вы похожи на спам-бота. Попробуйте отправить форму еще раз.');
         }
+        return true;
     }
 
 }
